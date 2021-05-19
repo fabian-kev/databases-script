2c2348dec2329222b7992c15565f7ed793ddf425
k.fabian <akame1v33@gmail.com>
2021-05-19 12:16:31 +0800

    lister and tns

--- oracle/commands.sql
+++ oracle/commands.sql
@@ -1,9 +1,14 @@
 
 -- to login to docker container as oracle user
 docker exec -it --user=oracle <container-name> bash
-
+docker exec -it --user=oracle oracle-18.4c bash
 -- to change allpassword of admin accounts
 docker exec oracle-18.4c /u01/app/oracle/setPassword.sh <password here>
 
 -- 18c oracle login as sysdba
 sqlplus sys@XEPDB1 as sysdba
+
+
+-- Create a user
+CREATE USER KEVINTEST IDENTIFIED BY KEVINTEST;
+GRANT ALL PRIVILEGES TO KEVINTEST;
